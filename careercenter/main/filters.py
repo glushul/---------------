365d49e350499f12e95e03a832012ba372f77309
glushul <ulyana.glushchenko2031@mail.ru>
2025-12-15 23:02:53 +0300

    five filters done: Search, Ordering, FilterSet, query_params.get(...), is_authentificated

--- careercenter/main/filters.py
+++ careercenter/main/filters.py
@@ -2,10 +2,7 @@ import django_filters
 from .models import Vacancy
 
 class VacancyFilter(django_filters.FilterSet):
-    is_active = django_filters.BooleanFilter(field_name="is_active")
-
-    company__industry = django_filters.CharFilter(lookup_expr='icontains')
-
+    
     salary_min = django_filters.NumberFilter(field_name='salary_min', lookup_expr='gte')
 
     title = django_filters.CharFilter(lookup_expr='icontains')
@@ -14,4 +11,4 @@ class VacancyFilter(django_filters.FilterSet):
 
     class Meta:
         model = Vacancy
-        fields = ['is_active']
+        fields = []
--- careercenter/main/views.py
+++ careercenter/main/views.py
@@ -1,8 +1,11 @@
-from django.http import HttpResponseBadRequest
 from django.shortcuts import get_object_or_404, redirect, render
-from django.core.exceptions import ObjectDoesNotExist
 from . import models, forms, serializers, pagination, filters
-from rest_framework import viewsets
+
+from django.http import HttpResponseBadRequest
+from django.core.exceptions import ObjectDoesNotExist
+
+from rest_framework import viewsets, filters as rest_framework_filters
+from django_filters.rest_framework import DjangoFilterBackend 
 
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = models.Company.objects.all()
@@ -13,7 +16,28 @@ class VacancyViewSet(viewsets.ModelViewSet):
     queryset = models.Vacancy.objects.all()
     serializer_class = serializers.VacancySerializer
     pagination_class = pagination.StandardResultsSetPagination
-    filterset_class = filters.VacancyFilter 
+    filterset_class = filters.VacancyFilter
+    filter_backends = [
+        DjangoFilterBackend,
+        rest_framework_filters.SearchFilter,
+        rest_framework_filters.OrderingFilter  
+    ]
+    search_fields = ['title', 'description', 'company__name']
+    ordering_fields = ['salary_max', 'salary_min']
+
+    def get_queryset(self):
+        qs = models.Vacancy.objects.all()
+        if self.request.query_params.get('my') == 'true':
+            if self.request.user.is_authenticated and hasattr(self.request.user, 'company'):
+                qs = qs.filter(company=self.request.user.company)
+            else:
+                return qs.none()
+
+        active_param = self.request.query_params.get('is_active')
+        if active_param is not None:
+            qs = qs.filter(is_active=(active_param.lower() == 'true'))
+
+        return qs
 
 def main(request):
     return redirect("vacancies")
